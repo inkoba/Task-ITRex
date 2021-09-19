4d225480d39930e26cc36997f4fbf4c6f8b7f51d
Koba <ikaborda.ik@gmail.com>
2021-09-19 23:54:51 +0300

    change all

--- js-script.js
+++ js-script.js
@@ -4,15 +4,14 @@ const getResours = async () => {
 
     return res.json();
 }
+let data = []
 
 
 const generationPosts = async () => {
     const data = await getResours();
-    const pagination = document.querySelector('#pagination');
+    //allData = await getResours();
 
-    const numPost = 20;
-    const countPage = Math.ceil(data.length / numPost);
-    const items = [];
+    filterDefaultByName(data);
 
     let showPage = (function () {
         let active;
@@ -42,7 +41,7 @@ const generationPosts = async () => {
             `;
             posts.forEach((post) => {
                 table.innerHTML += `
-                        <tr class='post' data-id="${post.id}">
+                        <tr class='post' data-id="${post.id}" data-num="${post.firstName}">
                             <td>${post.id}</td>
                             <td>${post.firstName}</td>
                             <td>${post.lastName}</td>
@@ -52,8 +51,14 @@ const generationPosts = async () => {
                         </tr>
                 `
             });
+
         };
     }());
+
+    const pagination = document.querySelector('#pagination');
+    const numPost = 20;
+    const countPage = Math.ceil(data.length / numPost);
+    const items = [];
 
     for (let i = 1; i <= countPage; i++) {
         let a = document.createElement('a');
@@ -65,115 +70,259 @@ const generationPosts = async () => {
 
     items.forEach(el => el.addEventListener('click', function () {
         showPage(el);
-    }))
+    }));
 
-}
-generationPosts();
+    // generation discription
+    const generatDiscriptionPost = async (arg, arg2) => {
 
+        const description = document.querySelector('#description');
+        description.innerHTML = "";
 
-//getting date-id, add class 
-const getId = (event) => {
-    event.preventDefault();
-    const description = document.querySelector('#description');
-    let profileId = 0;
+        data.forEach((post) => {
+            if (post.id == arg2 && post.firstName == arg) {
 
-    if (event.target.parentElement.classList.contains('post')) {
-        profileId = event.target.parentElement.dataset.id;
+                description.innerHTML += `
+                    <p>Profile info: </p>
+                    <p>Selected profile: ${post.firstName} ${post.lastName}</p>
+                    <p>Discription: ${post.description}</p>
+                    <p>Address: ${post.adress.streetAddress}</p>
+                    <p>City: ${post.adress.city}</p>
+                    <p>State: ${post.adress.state}</p>
+                    <p>Index: ${post.adress.zip}</p>
+                    `
+            };
+        });
     };
 
-    if (event.target.tagName === 'TD') {
-        description.classList.add('hide', 'style');
-    } else {
-        description.classList.remove('hide', 'style');
-    };
+    const getId = async (event) => {
+        event.preventDefault();
+        const description = document.querySelector('#description');
+        let profileNum;
+        let profileId = 0;
 
-    generatDiscriptionPost(profileId);
-
-}
-document.addEventListener('click', getId);
-
-//get data from the server and write to the description
-const generatDiscriptionPost = async (arg) => {
-    const data = await getResours();
-    const descript = document.querySelector('#description');
-    descript.innerHTML = "";
-    data.forEach((post, index) => {
-        if (post.id == arg) {
-            descript.innerHTML += `
-            <p>Profile info: </p>
-            <p>Selected profile: ${post.firstName} ${post.lastName}</p>
-            <p>Discription: ${post.description}</p>
-            <p>Address: ${post.adress.streetAddress}</p>
-            <p>City: ${post.adress.city}</p>
-            <p>State: ${post.adress.state}</p>
-            <p>Index: ${post.adress.zip}</p>
-        `
+        if (event.target.parentElement.classList.contains('post')) {
+            profileNum = event.target.parentElement.dataset.num;
         };
-    });
-};
 
-// table search
-const displaySearchTasks = async (allTasks) => {
-    const searchTasks = document.querySelector('#table');
-    searchTasks.innerHTML = `
-        <tr>
-            <th id="id1">id</th>
-            <th>First name</th>
-            <th>Last name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>State</th>
-        </tr>
-      `;
-    allTasks.forEach((post) => {
-        searchTasks.innerHTML += `
-            <tr class='post' data-id="${post.id}">
-                <td>${post.id}</td>
-                <td>${post.firstName}</td>
-                <td>${post.lastName}</td>
-                <td>${post.email}</td>
-                <td>${post.phone}</td>
-                <td>${post.adress.state}</td>
+        if (event.target.parentElement.classList.contains('post')) {
+            profileId = event.target.parentElement.dataset.id;
+        };
+
+        if (event.target.tagName === 'TD') {
+            description.classList.add('style')
+        } else { description.classList.remove('style') }
+
+        generatDiscriptionPost(profileNum, profileId);
+
+    }
+    document.addEventListener('click', getId);
+
+
+    // select
+    const selectFunc = async () => {
+
+        const option = document.querySelector('.select').value;
+        const filterTasks = data.filter((task) => {
+            if (task.adress.state === option) {
+                return task.adress.state;
+            };
+        })
+        console.log(filterTasks)
+        filterTasks.forEach(e => {
+
+            table.innerHTML = `
+                <tr>
+                <th id="id1">id</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>State</th>
+                </tr>
+            `
+            filterTasks.forEach((post) => {
+                table.innerHTML += `
+                <tr class='post' data-id="${post.id}" data-num="${post.firstName}">
+                    <td>${post.id}</td>
+                    <td>${post.firstName}</td>
+                    <td>${post.lastName}</td>
+                    <td>${post.email}</td>
+                    <td>${post.phone}</td>
+                    <td>${post.adress.state}</td>
+                </tr>
+        `
+            });
+        })
+        console.log(filterTasks)
+
+        filterByName(filterTasks)
+    }
+
+    document.querySelector('.select').addEventListener('change', selectFunc);
+
+
+    // table search
+    const displaySearchTasks = async (allTasks) => {
+        const searchTasks = document.querySelector('#table');
+        searchTasks.innerHTML = `
+            <tr>
+                <th id="id1">id</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>State</th>
             </tr>
-      `
+        `;
+        allTasks.forEach((post) => {
+            searchTasks.innerHTML += `
+                <tr class='post' data-id="${post.id}">
+                    <td>${post.id}</td>
+                    <td>${post.firstName}</td>
+                    <td>${post.lastName}</td>
+                    <td>${post.email}</td>
+                    <td>${post.phone}</td>
+                    <td>${post.adress.state}</td>
+                </tr>
+            `
+        })
+    }
+}
+
+function filterByName(filterTasks) {
+
+    document.querySelector(".search-input").addEventListener('input', (event) => {
+
+        const searchValue = event.target.value.toLowerCase().trim();
+
+        let searsh = [];
+        const table = document.querySelector('#table');
+        if (event.target.value && event.target.value !== '') {
+            searsh = [...filterTasks].filter(i => {
+
+                return i.firstName.toLowerCase().includes(searchValue)
+            })
+
+            searsh.forEach(e => {
+                table.innerHTML = `
+            <tr>
+                <th id="id1">id</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>State</th>
+            </tr>
+        `
+                searsh.forEach((post) => {
+                    table.innerHTML += `
+                    <tr class='post' data-id="${post.id}" data-num="${post.firstName}">
+                        <td>${post.id}</td>
+                        <td>${post.firstName}</td>
+                        <td>${post.lastName}</td>
+                        <td>${post.email}</td>
+                        <td>${post.phone}</td>
+                        <td>${post.adress.state}</td>
+                    </tr>
+            `
+                });
+            })
+
+        }
+
     })
 }
 
-const inputSearchTasks = async (event) => {
-    const searchValue = event.target.value.toLowerCase().trim();
-    const allTasks = await getResours();
+function filterDefaultByName(filterTasks) {
+    document.querySelector(".search-input").addEventListener('input', (event) => {
+        let currentData = filterTasks;
+        const table = document.querySelector('#table');
+        table.innerHTML = '';
 
-    const filterTasks = allTasks.filter((task) => {
-        if (task.firstName[0].toLowerCase().trim() === searchValue[0] || searchValue === "") {
-            return task.firstName.toLowerCase().includes(searchValue)
+        const searchValue = event.target.value.toLowerCase().trim();
+        currentData = filterTasks.filter(i => {
+            return i.firstName.toLowerCase().includes(searchValue)
+        })
+
+        currentData.forEach((post) => {
+            table.innerHTML += `
+                    <tr class='post' data-id="${post.id}" data-num="${post.firstName}">
+                        <td>${post.id}</td>
+                        <td>${post.firstName}</td>
+                        <td>${post.lastName}</td>
+                        <td>${post.email}</td>
+                        <td>${post.phone}</td>
+                        <td>${post.adress.state}</td>
+                    </tr>
+            `
+        });
+
+        let showPage = (function () {
+            let active;
+            const table = document.querySelector('#table');
+            return function (item) {
+
+                if (active) {
+                    active.classList.remove('active');
+                }
+                active = item;
+                item.classList.add('active');
+
+                let pageNum = Number(item.innerHTML);
+                let start = (pageNum - 1) * numPost;
+                let end = start + numPost;
+                let posts = currentData.slice(start, end);
+
+                table.innerHTML = `
+                <tr>
+                    <th id="id1">id</th>
+                    <th>First name</th>
+                    <th>Last name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>State</th>
+                </tr>
+            `;
+                posts.forEach((post) => {
+                    table.innerHTML += `
+                        <tr class='post' data-id="${post.id}" data-num="${post.firstName}">
+                            <td>${post.id}</td>
+                            <td>${post.firstName}</td>
+                            <td>${post.lastName}</td>
+                            <td>${post.email}</td>
+                            <td>${post.phone}</td>
+                            <td>${post.adress.state}</td>
+                        </tr>
+                `
+                });
+
+            };
+        }());
+
+
+        const pagination = document.querySelector('#pagination');
+        const numPost = 20;
+        const countPage = Math.ceil(currentData.length / numPost);
+        const items = [];
+
+        pagination.innerHTML = '';
+
+        for (let i = 1; i <= countPage; i++) {
+            let a = document.createElement('a');
+            a.innerHTML = i;
+            pagination.appendChild(a);
+            items.push(a);
         };
-    });
+        showPage(items[0]);
 
-    displaySearchTasks(filterTasks);
-};
-
-
-// input search
-if (document.querySelector(".search-input")) {
-    document.querySelector(".search-input").addEventListener("input", inputSearchTasks);
-};
-
-
-// select
-const selectFunc = async () => {
-    const option = document.querySelector('.select').value;
-    const allTasks = await getResours();
-
-    const filterTasks = allTasks.filter((task) => {
-        if (task.adress.state === option) {
-            return task.adress.state;
-        };
-    });
-
-    displaySearchTasks(filterTasks);
-
+        items.forEach(el => el.addEventListener('click', function () {
+            showPage(el);
+        }));
+    })
 }
-document.querySelector('.select').addEventListener('change', selectFunc);
+
+generationPosts();
+
 
 // sort table
 const sortTable = (index) => {
